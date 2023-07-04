5473a301592dc2868f0afc7ea1075c7b4214bbf4
Nobutaka Shiraki <shiraki.devi@gmail.com>
2023-07-04 11:39:34 +0900

    プロジェクトを新規追加

---
+++ ImageEx/app/src/main/java/com/example/imageex/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.imageex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.imageex.ui.theme.ImageExTheme
+
+// Imageコンポーネントを利用し画像を表示する処理を行う
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppScreen()
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun AppScreen() {
+
+}
