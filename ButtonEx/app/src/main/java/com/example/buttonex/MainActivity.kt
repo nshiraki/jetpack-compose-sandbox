ea6db70519a94135b36a7aaf42d73b069f7cbb68
Nobutaka Shiraki <shiraki.devi@gmail.com>
2023-07-04 23:04:57 +0900

    Buttonコンポーネントを利用した処理を作成

--- ButtonEx/app/src/main/java/com/example/buttonex/MainActivity.kt
+++ ButtonEx/app/src/main/java/com/example/buttonex/MainActivity.kt
@@ -3,9 +3,24 @@ package com.example.buttonex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-// Buttonコンポーネントを利用する処理を作成する
+// Buttonコンポーネントを利用した処理を作成する
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +31,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+@Preview(showBackground = true)
 fun AppScreen() {
+    // カウント数を格納
+    val counter = remember { mutableStateOf(0) }
 
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+        // カウント数の表示
+        Text(
+            color = Color.Black,
+            text = "count: ${counter.value}",
+            modifier = Modifier.padding(20.dp),
+            style = TextStyle(fontSize = 24.sp)
+        )
+
+        // カウントを加算するボタン
+        Button(
+            onClick = { counter.value++ }) {
+            Text(
+                text = "Count up", style = TextStyle(fontSize = 24.sp)
+            )
+        }
+    }
 }
