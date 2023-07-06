462d403d8d60d1ac8c7418ae87a51b650f35ce63
Nobutaka Shiraki <shiraki.devi@gmail.com>
2023-07-06 16:22:19 +0900

    遷移元から遷移先の画面へ引数を受け渡す処理を追加

--- NavigationComposeEx/app/src/main/java/com/example/navigationcomposeex/MainActivity.kt
+++ NavigationComposeEx/app/src/main/java/com/example/navigationcomposeex/MainActivity.kt
@@ -16,10 +16,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
+// Navigation Composeを利用して画面遷移の処理を行う
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,14 +44,24 @@ fun SetupNavHost() {
         // 画面遷移に関するコンポーザブルを登録
         composable(route = "screen1") {
             Screen1(
-                onButtonClick = {
+                onButtonClick = { text, id ->
                     // 特定の画面へ遷移
-                    navController.navigate("screen2")
+                    navController.navigate("screen2/$text/$id")
                 }
             )
         }
-        composable(route = "screen2") {
+        composable(
+            route = "screen2/{text}/{id}",
+            arguments = listOf(
+                navArgument("text") { type = NavType.StringType },
+                navArgument("id") { type = NavType.IntType },
+            )
+        ) { backStackEntry ->
+            val text = backStackEntry.arguments?.getString("text") ?: ""
+            val id = backStackEntry.arguments?.getInt("id") ?: 0
+
             Screen2(
+                text = text, id = id,
                 onButtonClick = {
                     // アプリ内の前の画面に戻る
                     navController.navigateUp()
@@ -60,30 +73,42 @@ fun SetupNavHost() {
 
 @Preview(showBackground = true)
 @Composable
-fun Screen1(onButtonClick: () -> Unit = {}) {
+fun Screen1(onButtonClick: (String, Int) -> Unit = { _, _ -> }) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
         Text(text = "Screen1", fontSize = 24.sp)
+
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = onButtonClick) {
-            Text(text = "Go to Screen2")
+
+        Button(onClick = { onButtonClick("Good morning", 1) }) {
+            Text(text = "1: Good morning")
+        }
+
+        Button(onClick = { onButtonClick("Good afternoon", 2) }) {
+            Text(text = "2: Good afternoon")
+        }
+
+        Button(onClick = { onButtonClick("Good evening", 3) }) {
+            Text(text = "3: Good afternoon")
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun Screen2(onButtonClick: () -> Unit = {}) {
+fun Screen2(text: String = "text", id: Int = 0, onButtonClick: () -> Unit = {}) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        Text(text = "Screen2", fontSize = 24.sp)
+        Text(text = "$id: $text", fontSize = 24.sp)
+
         Spacer(modifier = Modifier.height(16.dp))
+
         Button(onClick = onButtonClick) {
             Text(text = "Back to Screen1")
         }
