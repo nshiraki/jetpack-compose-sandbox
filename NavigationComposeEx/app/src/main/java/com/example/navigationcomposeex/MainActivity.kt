10a51adbd0285adee2f78170787ef2535f687c30
Nobutaka Shiraki <shiraki.devi@gmail.com>
2023-07-06 12:08:12 +0900

    遷移する画面を追加

--- NavigationComposeEx/app/src/main/java/com/example/navigationcomposeex/MainActivity.kt
+++ NavigationComposeEx/app/src/main/java/com/example/navigationcomposeex/MainActivity.kt
@@ -3,8 +3,19 @@ package com.example.navigationcomposeex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,4 +30,36 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppScreen() {
     // TODO: "Not yet implemented"
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Screen1(onClickButton: () -> Unit = {}) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(text = "Screen1", fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onClickButton) {
+            Text(text = "Go to Screen2")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Screen2(onClickButton: () -> Unit = {}) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(text = "Screen2", fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onClickButton) {
+            Text(text = "Back to Screen1")
+        }
+    }
 }
