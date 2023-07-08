8caeebcafd2e2ea5b4a4e3c99df8ea2bc50cb2e4
Nobutaka Shiraki <shiraki.devi@gmail.com>
2023-07-08 22:57:52 +0900

    プロジェクトを新規作成

---
+++ AlertDialogEx/app/src/main/java/com/example/alertdialogex/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.alertdialogex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.alertdialogex.ui.theme.AlertDialogExTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppScreen()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppScreen() {
+    // TODO: Not yet implemented
+}
